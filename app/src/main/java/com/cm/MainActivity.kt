116c17d96c9f33a5e962e039d37e876a23401399
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-07-20 13:44:48 +0700

    Refactor MainActivity and PointAdapter for clarity

--- app/src/main/java/com/cm/MainActivity.kt
+++ app/src/main/java/com/cm/MainActivity.kt
@@ -1,19 +1,24 @@
-package com.cm.autobid
+package com.cm
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.cm.autobid.databinding.ActivityMainBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.cm.databinding.ActivityMainBinding
 
-data class Point(val min: Int, val max: Int, val jarak: Int) // Data class buat nyimpen 1 aturan
+data class Point(val min: Int, val max: Int, val jarak: Int)
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var isRunning = false
-    private val listPoint = ArrayList<Point>() // List buat nyimpen semua pengaturan
-    private lateinit var adapter: PointAdapter // Adapter RecyclerView
+    private val listPoint = ArrayList<Point>()
+    private lateinit var adapter: PointAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,8 +37,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupButtons() {
         binding.btnAddPoint.setOnClickListener {
-            val interval = binding.etInterval.text.toString()
-            val delay = binding.etDelay.text.toString()
             val min = binding.etMin.text.toString().toIntOrNull()?: 0
             val max = binding.etMax.text.toString().toIntOrNull()?: 0
             val jarak = binding.etJarak.text.toString().toIntOrNull()?: 0
@@ -43,10 +46,9 @@ class MainActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            listPoint.add(Point(min, max, jarak)) // Tambah ke list
-            adapter.notifyDataSetChanged() // Update RecyclerView
+            listPoint.add(Point(min, max, jarak))
+            adapter.notifyItemInserted(listPoint.size - 1)
 
-            // Kosongkan field
             binding.etMin.text.clear()
             binding.etMax.text.clear()
             binding.etJarak.text.clear()
@@ -58,32 +60,33 @@ class MainActivity : AppCompatActivity() {
             isRunning =!isRunning
             if (isRunning) {
                 binding.tvStatus.text = "RUNNING"
-                binding.tvStatus.setTextColor(resources.getColor(android.R.color.holo_green_light))
+                binding.tvStatus.setTextColor(getColor(android.R.color.holo_green_light))
                 binding.btnStart.text = " STOP"
                 binding.btnStart.setCompoundDrawablesWithIntrinsicBounds(android.R.drawable.ic_media_pause, 0, 0, 0)
-                binding.btnStart.backgroundTintList = resources.getColorStateList(android.R.color.holo_red_dark)
+                binding.btnStart.backgroundTintList = getColorStateList(android.R.color.holo_red_dark)
             } else {
                 binding.tvStatus.text = "STOP"
-                binding.tvStatus.setTextColor(resources.getColor(android.R.color.holo_red_light))
+                binding.tvStatus.setTextColor(getColor(android.R.color.holo_red_light))
                 binding.btnStart.text = " MULAI"
                 binding.btnStart.setCompoundDrawablesWithIntrinsicBounds(android.R.drawable.ic_media_play, 0, 0, 0)
-                binding.btnStart.backgroundTintList = resources.getColorStateList(android.R.color.holo_green_dark)
+                binding.btnStart.backgroundTintList = getColorStateList(android.R.color.holo_green_dark)
             }
         }
     }
 }
 
-// Adapter sederhana buat nampilin list
-class PointAdapter(private val list: ArrayList<Point>) : androidx.recyclerview.widget.RecyclerView.Adapter<PointAdapter.ViewHolder>() {
-    class ViewHolder(val binding: android.view.View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(binding)
-    override fun onCreateViewHolder(parent: android.view.ViewGroup, viewType: Int): ViewHolder {
-        val view = android.view.LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+class PointAdapter(private val list: ArrayList<Point>) : RecyclerView.Adapter<PointAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tv: TextView = itemView.findViewById(android.R.id.text1)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
         return ViewHolder(view)
     }
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = list[position]
-        (holder.binding as android.widget.TextView).text = "Rp${item.min} - Rp${item.max} | ${item.jarak} Meter"
-        holder.binding.setPadding(16,16,16,16)
+        holder.tv.text = "Rp${item.min} - Rp${item.max} | ${item.jarak} Meter"
+        holder.tv.setPadding(16,16,16,16)
     }
     override fun getItemCount(): Int = list.size
 }
