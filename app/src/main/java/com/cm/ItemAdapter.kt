2e315d129cb424826a8f8cd261b67f3e1872206f
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-07-20 14:49:46 +0700

    Add ItemAdapter for RecyclerView implementation

---
+++ app/src/main/java/com/cm/ItemAdapter.kt
@@ -0,0 +1,62 @@
+package com.cm
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cm.databinding.ItemPointBinding
+import com.cm.model.Point
+
+
+class ItemAdapter(
+    private val items:ArrayList<Point>
+):
+RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+
+    class ViewHolder(
+        val binding: ItemPointBinding
+    ):
+    RecyclerView.ViewHolder(binding.root)
+
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType:Int
+    ):ViewHolder {
+
+
+        val binding =
+            ItemPointBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+
+
+        return ViewHolder(binding)
+    }
+
+
+
+    override fun onBindViewHolder(
+        holder:ViewHolder,
+        position:Int
+    ){
+
+        val item = items[position]
+
+
+        holder.binding.tvItem.text =
+            "Rp${item.min} - Rp${item.max} | ${item.jarak} Meter"
+
+    }
+
+
+
+    override fun getItemCount():Int{
+        return items.size
+    }
+
+}
