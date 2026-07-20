0ceb49f3859aac30b82c0b95264b33383245b6ba
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-07-20 14:03:46 +0700

    Refactor build.gradle.kts syntax and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,59 +1,37 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id 'com.android.application'
+    id 'org.jetbrains.kotlin.android'
 }
 
 android {
-    namespace = "com.cm"
-    compileSdk = 34
+    namespace 'com.cm'
+    compileSdk 34
 
     defaultConfig {
-        applicationId = "com.cm"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        applicationId "com.cm"
+        minSdk 24
+        targetSdk 34
+        versionCode 1
+        versionName "1.0"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
         }
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
+    
+    // TAMBAHIN INI
     buildFeatures {
-        viewBinding = true
+        viewBinding true
     }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.13.1")
-
-    implementation("androidx.appcompat:appcompat:1.7.0")
-
-    implementation("com.google.android.material:material:1.12.0")
-
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-
-    testImplementation("junit:junit:4.13.2")
-
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation 'androidx.core:core-ktx:1.12.0'
+    implementation 'androidx.appcompat:appcompat:1.6.1'
+    implementation 'com.google.android.material:material:1.11.0'
+    implementation 'androidx.recyclerview:recyclerview:1.3.2' // buat list
+    implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
 }
