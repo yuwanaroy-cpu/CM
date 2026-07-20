a185ce0053114c5740c73e20122f4ff21d3048dd
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-07-20 17:26:47 +0700

    Update CMAccessibilityService.kt

--- app/src/main/java/com/cm/service/CMAccessibilityService.kt
+++ app/src/main/java/com/cm/service/CMAccessibilityService.kt
@@ -1,29 +1,217 @@
 package com.cm.service
 
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.GestureDescription
+import android.graphics.Path
+import android.os.Handler
+import android.os.Looper
 import android.view.accessibility.AccessibilityEvent
+import com.cm.model.Point
 
 class CMAccessibilityService : AccessibilityService() {
 
+
     companion object {
+
         var instance: CMAccessibilityService? = null
+
     }
+
+
+    private val handler =
+        Handler(Looper.getMainLooper())
+
+
+    private var pointList: List<Point> =
+        emptyList()
+
+
+    private var currentIndex = 0
+
+
+    private var isRunning = false
+
+
 
     override fun onServiceConnected() {
+
         super.onServiceConnected()
+
         instance = this
+
     }
+
+
+
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        // Akan kita gunakan nanti
+
     }
+
+
+
 
     override fun onInterrupt() {
-        // Akan kita gunakan nanti
+
+        stopService()
+
     }
 
+
+
+
+
     override fun onDestroy() {
+
         super.onDestroy()
+
+        stopService()
+
         instance = null
+
     }
+
+
+
+
+
+    fun startService(points: List<Point>) {
+
+
+        if (points.isEmpty()) return
+
+
+        pointList = points
+
+
+        currentIndex = 0
+
+
+        isRunning = true
+
+
+        runAutoClick()
+
+    }
+
+
+
+
+
+
+    fun stopService() {
+
+
+        isRunning = false
+
+
+        handler.removeCallbacksAndMessages(null)
+
+    }
+
+
+
+
+
+
+
+    private fun runAutoClick() {
+
+
+        if (!isRunning) return
+
+
+        if (pointList.isEmpty()) return
+
+
+
+        val point =
+            pointList[currentIndex]
+
+
+
+        performClick(
+            point.x,
+            point.y
+        )
+
+
+
+        currentIndex =
+            (currentIndex + 1) % pointList.size
+
+
+
+
+        handler.postDelayed(
+
+            {
+
+                runAutoClick()
+
+            },
+
+            point.interval.toLong()
+
+        )
+
+    }
+
+
+
+
+
+
+
+    private fun performClick(
+        x: Int,
+        y: Int
+    ) {
+
+
+        val path = Path()
+
+
+        path.moveTo(
+            x.toFloat(),
+            y.toFloat()
+        )
+
+
+
+        val gesture =
+            GestureDescription.Builder()
+
+
+                .addStroke(
+
+                    GestureDescription.StrokeDescription(
+
+                        path,
+
+                        0,
+
+                        100
+
+                    )
+
+                )
+
+
+                .build()
+
+
+
+        dispatchGesture(
+
+            gesture,
+
+            null,
+
+            null
+
+        )
+
+    }
+
 }
