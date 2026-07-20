c584e96b42ad719073818081263cbf7b534032ef
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-07-20 22:25:04 +0700

    Refactor ItemAdapter to use listener instead of onDelete

--- app/src/main/java/com/yuwanaroy/cpu/cmbid/ItemAdapter.kt
+++ app/src/main/java/com/yuwanaroy/cpu/cmbid/ItemAdapter.kt
@@ -6,14 +6,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.yuwanaroy.cpu.cmbid.databinding.ItemRowBinding
 import com.yuwanaroy.cpu.cmbid.model.Point
 
-class ItemAdapter(private val list: MutableList<Point>, private val onDelete: (Int) -> Unit) : 
-    RecyclerView.Adapter<ItemAdapter.VH>() {
+class ItemAdapter(
+    private val list: MutableList<Point>, 
+    private val listener: (Int) -> Unit
+) : RecyclerView.Adapter<ItemAdapter.VH>() {
+
     inner class VH(val b: ItemRowBinding) : RecyclerView.ViewHolder(b.root)
-    override fun onCreateViewHolder(p: ViewGroup, v: Int) = VH(ItemRowBinding.inflate(LayoutInflater.from(p.context), p, false))
-    override fun onBindViewHolder(h: VH, pos: Int) {
-        val i = list[pos]
-        h.b.tvInterval.text = "I:${i.interval} D:${i.delay} H:${i.minHarga}-${i.maxHarga} J:${i.jarak}km"
-        h.b.btnDelete.setOnClickListener { onDelete(pos) }
+
+    override fun onCreateViewHolder(p: ViewGroup, v: Int): VH {
+        val binding = ItemRowBinding.inflate(LayoutInflater.from(p.context), p, false)
+        return VH(binding)
     }
+
+    override fun onBindViewHolder(h: VH, pos: Int) {
+        val item = list[pos]
+        h.b.tvInterval.text = "${item.interval} ms" // SESUAI LAYOUT KAMU
+        h.b.btnDelete.setOnClickListener {
+            listener(pos)
+        }
+    }
+
     override fun getItemCount() = list.size
 }
