24f34cfc1e534d464ef9a0b68682bbc7ca117bda
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-07-20 20:42:14 +0700

    Add Point data class with multiple properties

---
+++ app/src/main/java/com/yuwanaroy/cpu/cmbid/model/Point.kt
@@ -0,0 +1,11 @@
+package com.yuwanaroy.cpu.cmbid.model
+
+data class Point(
+    val x: Int,
+    val y: Int,
+    val interval: Int,
+    val delay: Int,
+    val minHarga: Int,
+    val maxHarga: Int,
+    val jarak: Int
+)
