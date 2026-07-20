767015c4a8b88d3d24e6ec5389d248f464447e47
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-07-20 22:28:15 +0700

    Modify Point properties for mutability and type change
    Changed properties x and y from val to var and updated interval and delay types from Int to Long.

--- app/src/main/java/com/yuwanaroy/cpu/cmbid/model/Point.kt
+++ app/src/main/java/com/yuwanaroy/cpu/cmbid/model/Point.kt
@@ -1,10 +1,10 @@
 package com.yuwanaroy.cpu.cmbid.model
 
 data class Point(
-    val x: Int,
-    val y: Int,
-    val interval: Int,
-    val delay: Int,
+    var x: Int,
+    var y: Int,
+    val interval: Long,
+    val delay: Long,
     val minHarga: Int,
     val maxHarga: Int,
     val jarak: Int
