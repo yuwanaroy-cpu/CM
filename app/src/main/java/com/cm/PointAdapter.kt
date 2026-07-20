b54b360218ca122000918664f5943f758d12c900
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-07-20 20:06:07 +0700

    Update PointAdapter.kt
    Updated package names and added click listener to remove items.

--- app/src/main/java/com/cm/PointAdapter.kt
+++ app/src/main/java/com/cm/PointAdapter.kt
@@ -1,10 +1,10 @@
-package com.cm
+package com.yuwanaroy.cpu.cmbid // <-- GANTI INI
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.cm.databinding.ItemRowBinding
-import com.cm.model.Point
+import com.yuwanaroy.cpu.cmbid.databinding.ItemRowBinding // <-- GANTI INI
+import com.yuwanaroy.cpu.cmbid.model.Point // <-- GANTI INI
 import java.util.ArrayList
 
 class PointAdapter(private val list: ArrayList<Point>) :
@@ -20,6 +20,13 @@ class PointAdapter(private val list: ArrayList<Point>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = list[position]
         holder.binding.tvItem.text = "I:${item.interval}ms D:${item.delay}ms | Rp${item.minHarga}-${item.maxHarga} | ${item.jarak}m"
+
+        // TAMBAHIN INI BIAR BISA HAPUS PAS DI KLIK
+        holder.binding.root.setOnClickListener {
+            list.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, list.size)
+        }
     }
 
     override fun getItemCount(): Int = list.size
