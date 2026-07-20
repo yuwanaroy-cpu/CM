2c5b6b0b10fdbe14497ef88bcd3cbf0ac6f6c802
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-07-20 20:20:46 +0700

    Update build.gradle.kts
    Updated namespace and applicationId for the project. Adjusted Kotlin and Java compatibility versions, and modified dependencies to use updated versions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,45 +4,55 @@ plugins {
 }
 
 android {
-    namespace = "com.cm"
+    namespace = "com.yuwanaroy.cpu.cmbid" // <-- INI FIX ERROR NAMESPACE TADI
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.cm"
-        minSdk = 24
+        applicationId = "com.yuwanaroy.cpu.cmbid"
+        minSdk = 24  // Android 7.0 ke atas
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        debug {
+            isDebuggable = true
         }
     }
-
-    buildFeatures {
-        viewBinding = true
-    }
-
+    
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
-
+    
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
+    }
+    
+    buildFeatures {
+        viewBinding = true // <-- PENTING biar MainActivity bisa pakai binding
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2") // <-- Buat daftar point
+    
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
