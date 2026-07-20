799764cc9467bc0715a6629c458e142132a18491
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-07-20 14:13:39 +0700

    Add Java and Kotlin compatibility options

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,15 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
