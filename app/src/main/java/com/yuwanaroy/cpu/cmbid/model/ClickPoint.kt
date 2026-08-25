9b932956376f5623299be2fa07287e109a4f5fb0
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-08-26 01:03:18 +0700

    Change delay property to mutable in ClickPoint
    Changed 'delay' property from 'val' to 'var' to allow modification.

--- app/src/main/java/com/yuwanaroy/cpu/cmbid/model/ClickPoint.kt
+++ app/src/main/java/com/yuwanaroy/cpu/cmbid/model/ClickPoint.kt
@@ -3,5 +3,5 @@ package com.yuwanaroy.cpu.cmbid.model
 data class ClickPoint(
     val x: Float,
     val y: Float,
-    val delay: Long = 1000L
+    var delay: Long = 1000L // Ubah 'val' menjadi 'var' di sini
 )
