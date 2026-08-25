39512b7004602d3f63c65247184929fcac8f3e58
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-08-26 00:50:05 +0700

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 android {
-    namespace = "com.yuwanaroy.cpu.cmbid" // <-- INI FIX ERROR NAMESPACE TADI
+    namespace = "com.yuwanaroy.cpu.cmbid"
     compileSdk = 34
 
     defaultConfig {
         applicationId = "com.yuwanaroy.cpu.cmbid"
-        minSdk = 24  // Android 7.0 ke atas
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -29,29 +29,31 @@ android {
             isDebuggable = true
         }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    
+
     buildFeatures {
-        viewBinding = true // <-- PENTING biar MainActivity bisa pakai binding
+        viewBinding = true
     }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.2") // <-- Buat daftar point
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     
+    // PERBAIKAN: Dependency Gson ditambahkan di sini
+    implementation("com.google.code.gson:gson:2.10.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
