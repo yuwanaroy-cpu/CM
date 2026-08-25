a5d152d480f2da40d699c3a932c35a2e25314686
yuwanaroy-cpu <yuwanaroy@gmail.com>
2026-08-25 22:00:58 +0700

    Update BootReceiver to launch MainActivity on boot
    Changed the package declaration and updated the action on boot completion to start MainActivity instead of CMForegroundService.

--- app/src/main/java/com/yuwanaroy/cpu/cmbid/BootReceiver.kt
+++ app/src/main/java/com/yuwanaroy/cpu/cmbid/BootReceiver.kt
@@ -1,15 +1,15 @@
-package com.yuwanaroy.cpu.cmbid.receiver
+package com.yuwanaroy.cpu.cmbid
+
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.yuwanaroy.cpu.cmbid.service.CMForegroundService
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            val i = Intent(context, CMForegroundService::class.java)
-            i.action = CMForegroundService.ACTION_START
-            context.startForegroundService(i)
+            val startIntent = Intent(context, MainActivity::class.java)
+            startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(startIntent)
         }
     }
 }
